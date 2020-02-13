67e1c76dda0fbfcfc4da8fd9a3f5d93f2f66f094
Ferney Arenas <arenas.mauricio@yahoo.com>
2020-02-13 21:17:09 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -77,5 +77,78 @@ end
  
  
  
- 
+ require 'pry'
+#Here is the song hash you will be working with. Each key is a song name and each value is the location of it's mp3 file.
+#make sure to edit the value of each key to replaceUsers/Sam/Development/code
+#with the correUsers/Sam/Development/coden your computer
+
+
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : lets you choose a song to play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+
+
+
+def list(my_songs)
+  #this method is different! Collect the keys of the my_songs hash and 
+  #list the songs by name
+  i = 1
+   my_songs.each_key do |song|
+    puts "#{i}. #{song}"
+    i += 1
+  end
+end
+
+
+def play(my_songs)
+  #this method is slightly different!
+  #you should still ask the user for input and collect their song choice
+  #this time, only allow user's to input a song name
+  #check to see if the name they give is in fact a key of the my_songs hash
+  #if it isn't, tell them their choice is invalid
+  #if it is, play the song using the system 'open <file path>' syntax
+  #get the file path of the song by looking it up in the my_songs hash
+  puts "Please enter a song name:"
+  input = gets.chomp
+  my_songs.each_key do |song|
+    if input == song.downcase
+      system "open " + my_songs[song]
+      break
+    end 
+
+  end
+end
+
+def exit_jukebox
+  #this method is the same as in jukebox.rb
+  puts "Goodbye"
+end
+
+def run(my_songs)
+  #this method is the same as in jukebox.rb
+  help
+  input = ""
+  while input != "exit"
+    puts "Please enter a command:"
+    input = gets.chomp
+    case input
+    when "play"
+      play(my_songs)
+    when "list"
+      list(my_songs)
+    when "help"
+      help
+    when "exit"
+      exit_jukebox    
+    else
+      puts "Invalid input, please try again"
+    end
+  end
+end
  
